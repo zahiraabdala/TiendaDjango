a81beadd18598a47c7d1576777a1bdcc1b71bc2e
zahira <zahiraabdala@gmail.com>
2021-07-01 18:21:23 -0300

    configuraciones de producto

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Producto
 
 # Register your models here.
+admin.site.register(Producto)
+admin.site.register(Categoria)
+
--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=80, null=False)
+
+    def __str__(self):
+        return self.descripcion
+
+class Producto(models.Model):
+    titulo       = models.CharField(max_length=500, null=False)
+    imagen       = models.ImageField(upload_to='imagenes/')
+    descripcion  = models.CharField(max_length=2500, null=False)
+    precio       = models.DecimalField(decimal_places=2, max_digits=50, default=0.00)
+    categoria   = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
