ce05987bce695465c6bdcd773bbe9f7e5fe5277f
zahira <zahiraabdala@gmail.com>
2021-07-01 21:33:52 -0300

    index layout

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Categoria, Producto #importo las clases de models, tanto de producto como de categoria
 
 # Create your views here.
 def index(request):
-    return render(request, "layout.html", {})
+    variableProducto = Producto.objects.all()[0:3] #genero la variable "variableProducto" y me traigo todo (all) lo que tengo en la clase producto
+    variableProducto2 = Producto.objects.all()[3:10]
+    return render(request, "index.html", {
+        'variableProducto':variableProducto,
+        'variableProducto2':variableProducto2,})
